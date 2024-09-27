d1a67696a8c6607fcebabf031e6e01c8b963e3f0
amb2007 <agustinmancinibobadilla@gmail.com>
2024-09-27 12:20:50 -0300

    falta footer estatico

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-import Portada from "./components/Portada";
 import Footer from "./components/Footer";
-import axios from "axios";
-
-const options = {
-	method: "GET",
-	url: "https://online-movie-database.p.rapidapi.com/auto-complete",
-	params: { q: "john wick" },
-	headers: {
-		"x-rapidapi-key": "cb71281a97msh036212d3e3bc848p18e3e2jsn541ec39839d0",
-		"x-rapidapi-host": "online-movie-database.p.rapidapi.com",
-	},
-};
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+import Portada from "./components/Portada";
 
 function App() {
 	return (
 		<>
-			<h1>hola</h1>
-			{/* <Portada />
-			<Footer/> */}
+			 <Portada />
+			 <Footer/> 
 		</>
 	);
 }
