2be1d182543f90b401090024177d9890fd5ff111
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-03 11:24:18 -0300

    a reparar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import "./App.css";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-
-
-
-=======
 import Portada from "./components/Portada";
->>>>>>> d1a67696a8c6607fcebabf031e6e01c8b963e3f0
+import Navbar from "./components/Navbar";
+
 
 function App() {
 	return (
 		<>
-<<<<<<< HEAD
-			<h1>hola</h1>
-			<Portada></Portada>
-			<Footer></Footer>
-=======
-			 <Portada />
-			 <Footer/> 
->>>>>>> d1a67696a8c6607fcebabf031e6e01c8b963e3f0
+			<Navbar />
+			<Portada />
+			<Footer />
 		</>
 	);
 }
