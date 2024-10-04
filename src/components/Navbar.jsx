31ac505461acb3e80869eff5529652c2276c789d
JosiasAlvaredo <135233972+JosiasAlvaredo@users.noreply.github.com>
2024-10-04 10:54:08 -0300

    a reparar (aun)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
+import React, { useState, useEffect,  useRef } from 'react';
+import axios from 'axios';
 
 function Navbar() {
-    let checkList = document.getElementById('list1');
-    checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
-        if (checkList.classList.contains('visible'))
-            checkList.classList.remove('visible');
-        else
-            checkList.classList.add('visible');
-    }
+    const [genres, setGenres] = useState([]);
+    const [visible, setVisible] = useState(false);
+    const listRef = useRef(null);
+
+    useEffect(() => {
+        axios.request({
+            method: 'POST',
+            url: 'https://online-movie-database.p.rapidapi.com/v2/search-advance',
+            headers: {
+                'x-rapidapi-key': 'Sign Up for Key',
+                'x-rapidapi-host': 'online-movie-database.p.rapidapi.com',
+                'Content-Type': 'application/json'
+            },
+            data: {
+                allGenreIds: [] 
+            }
+        })
+            .then(response => {
+                const genres = response.data.allGenreIds;
+                setGenres(genres);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+    const handleToggle = () => {
+        setVisible(!visible);
+    };
 
     return (
         <nav className="navbar">
             <h1>Filtro</h1>
-            <div id="list1" class="dropdown-check-list" tabindex="100">
-                <span class="anchor">Seleccione una categoria</span>
-                <ul class="items">
-                    <li><input type="checkbox" />Comedia </li>
-                    <li><input type="checkbox" />Horror</li>
-                    <li><input type="checkbox" />Romance </li>
-                    <li><input type="checkbox" />Suspenso </li>
-                    <li><input type="checkbox" />Ciencia ficción </li>
-                    <li><input type="checkbox" />Drama </li>
-                    <li><input type="checkbox" />Acción</li>
-                    <li><input type="checkbox" />Aventura</li>
-                    <li><input type="checkbox" />Animación</li>
-                    <li><input type="checkbox" />Biografía</li>
-                    <li><input type="checkbox" />Crimen</li>
-                    <li><input type="checkbox" />Documental</li>
-                    <li><input type="checkbox" />Familiar</li>
-                    <li><input type="checkbox" />Fantasía</li>
-                    <li><input type="checkbox" />Film-Noir</li>
-                    <li><input type="checkbox" />Game-Show</li>
-                    <li><input type="checkbox" />Historia</li>
-                    <li><input type="checkbox" />Musical</li>
-                    <li><input type="checkbox" />Música</li>
-                    <li><input type="checkbox" />Misterio</li>
-                    <li><input type="checkbox" />Caballeriza</li>
-                    <li><input type="checkbox" />Reality-TV</li>
-                    <li><input type="checkbox" />Deporte</li>
-                    <li><input type="checkbox" />Programa de entrevistas</li>
-                    <li><input type="checkbox" />Guerra</li>
-                    <li><input type="checkbox" />Occidental</li>
+            <div id="list1" className="dropdown-check-list" ref={listRef}>
+                <span className="anchor" onClick={handleToggle}>
+                    Seleccione un género
+                </span>
+                <ul className="items">
+                    {genres.map(genre => (
+                        <li key={genre}>
+                            <input type="checkbox" /> {genre.name}
+                        </li>
+                    ))}
                 </ul>
             </div>
         </nav>
