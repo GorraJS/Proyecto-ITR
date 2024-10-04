87485280ce996a9b2a23a8f0bcdf874732fca183
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-04 19:40:21 -0300

    molde de navbar listo...p/d esperare a la API para modificarla nuevamente

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,30 @@
-import React, { useState, useEffect,  useRef } from 'react';
-import axios from 'axios';
-
 function Navbar() {
-    const [genres, setGenres] = useState([]);
-    const [visible, setVisible] = useState(false);
-    const listRef = useRef(null);
-
-    useEffect(() => {
-        axios.request({
-            method: 'POST',
-            url: 'https://online-movie-database.p.rapidapi.com/v2/search-advance',
-            headers: {
-                'x-rapidapi-key': 'Sign Up for Key',
-                'x-rapidapi-host': 'online-movie-database.p.rapidapi.com',
-                'Content-Type': 'application/json'
-            },
-            data: {
-                allGenreIds: [] 
-            }
-        })
-            .then(response => {
-                const genres = response.data.allGenreIds;
-                setGenres(genres);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
-
-    const handleToggle = () => {
-        setVisible(!visible);
-    };
-
     return (
-        <nav className="navbar">
-            <h1>Filtro</h1>
-            <div id="list1" className="dropdown-check-list" ref={listRef}>
-                <span className="anchor" onClick={handleToggle}>
-                    Seleccione un género
-                </span>
-                <ul className="items">
-                    {genres.map(genre => (
-                        <li key={genre}>
-                            <input type="checkbox" /> {genre.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </nav>
+        <>
+            <img id="logo" src="INCLUIR_RUTA_DE_IMAGEN" ></img>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Preguntas</a></li>
+                <li>
+                    <div className="dropdown">
+                        <a href="#" className="dropdown-toggle">Géneros</a>
+                        <ul className="dropdown-menu">
+                            <li>
+                                <input type="checkbox" />Acción
+                            </li>
+                            <li>
+                                
+                                <input type="checkbox" />Comedia
+                            </li>
+                            <li>
+                                <input type="checkbox" />Terror
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </>
     );
-};
+}
 
 export default Navbar;
