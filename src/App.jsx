3d4232c2cb8d54136640c7066e39fc99a8ec878e
Gorra.js <gorrahabla1234@gmail.com>
2024-10-04 10:16:15 -0300

    version para vercel v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import './App.css'
-import axios from 'axios';
-
-  const options = {
-    method: 'GET',
-    url: 'https://online-movie-database.p.rapidapi.com/auto-complete',
-    params: {q: 'john wick'},
-    headers: {
-      'x-rapidapi-key': 'cb71281a97msh036212d3e3bc848p18e3e2jsn541ec39839d0',
-      'x-rapidapi-host': 'online-movie-database.p.rapidapi.com'
-    }
-  };
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
-=======
 import "./App.css";
 import Footer from "./components/Footer";
 import Portada from "./components/Portada";
 import Navbar from "./components/Navbar";
 
->>>>>>> REACT
 
 function App() {
 	return (
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import { createRoot } from 'react-dom/client'
 import Footer from "./components/Footer";
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
-=======
 import axios from 'axios';
 
->>>>>>> REACT
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
