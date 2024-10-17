63d4061a81e1225b62b9e77ee3942314cb902444
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-17 10:44:56 -0300

    Ingles actualizado y Navbar css a pulir

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,32 @@
+import './Navbar.css';
+
 function Navbar() {
     return (
         <>
             <img id="logo" src="INCLUIR_RUTA_DE_IMAGEN" ></img>
             <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Preguntas</a></li>
+                <li><a href="#">Home</a></li>
                 <li>
                     <div className="dropdown">
-                        <a href="#" className="dropdown-toggle">Géneros</a>
+                        <a href="#" className="dropdown-toggle">Genders</a>
                         <ul className="dropdown-menu">
                             <li>
-                                <input type="checkbox" />Acción
+                                <input type="checkbox" />Action
                             </li>
                             <li>
-                                <input type="checkbox" />Comedia
+                                <input type="checkbox" />Comedy
                             </li>
                             <li>
-                                <input type="checkbox" />Terror
+                                <input type="checkbox" />Horror
                             </li>
                         </ul>
                     </div>
                 </li>
+                <li><a href="#">Questions</a></li>
+                <li><a href="#">Front page</a></li>
+                <li><a href="#">Trailers</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Favorites</a></li>
             </ul>
         </>
     );
