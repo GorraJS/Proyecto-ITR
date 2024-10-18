b571eb609800c3306ff78888e1b8fe3e36d6bc6f
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-18 10:33:32 -0300

    navbar finalizada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,27 +6,13 @@ function Navbar() {
             <img id="logo" src="INCLUIR_RUTA_DE_IMAGEN" ></img>
             <ul>
                 <li><a href="#">Home</a></li>
-                <li>
-                    <div className="dropdown">
-                        <a href="#" className="dropdown-toggle">Genders</a>
-                        <ul className="dropdown-menu">
-                            <li>
-                                <input type="checkbox" />Action
-                            </li>
-                            <li>
-                                <input type="checkbox" />Comedy
-                            </li>
-                            <li>
-                                <input type="checkbox" />Horror
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+                <li><a href="#">Genders</a></li>
                 <li><a href="#">Questions</a></li>
                 <li><a href="#">Front page</a></li>
                 <li><a href="#">Trailers</a></li>
                 <li><a href="#">Movies</a></li>
                 <li><a href="#">Favorites</a></li>
+                
             </ul>
         </>
     );
