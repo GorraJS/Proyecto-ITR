b567cbb08ff17d3318f36156ab7751e9a5debde7
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-18 10:55:48 -0300

    navbar finalizada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,13 @@ function Navbar() {
         <>
             <img id="logo" src="INCLUIR_RUTA_DE_IMAGEN" ></img>
             <ul>
-                <li><a href="#">Home</a></li>
                 <li><a href="#">Genders</a></li>
                 <li><a href="#">Questions</a></li>
                 <li><a href="#">Front page</a></li>
                 <li><a href="#">Trailers</a></li>
                 <li><a href="#">Movies</a></li>
                 <li><a href="#">Favorites</a></li>
-                
+                <li><a href="#">Home</a></li>
             </ul>
         </>
     );
